285a7e29993262b5cdef089ebda106773ea1a3c7
Tapiwa128 <106350924+Tapiwa128@users.noreply.github.com>
2025-06-12 19:11:21 +0200

    Update Home.jsx

--- brutusandreas/src/pages/home/Home.jsx
+++ brutusandreas/src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css"; 
 import TopHeader from "../../components/TopHeader/TopHeader";
+import AboutSection from "../../components/AboutSection/AboutSection";
 import Showcase from "../../components/Showcase/Showcase";
 import Footer from "../../components/Footer/Footer";
 

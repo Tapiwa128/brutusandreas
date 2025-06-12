7b6a08d31ee9d0f0d0dd6400cd372188f04270ef
Tapiwa128 <106350924+Tapiwa128@users.noreply.github.com>
2025-06-12 16:12:48 +0200

    Navbar

--- brutusandreas/src/pages/home/Home.jsx
+++ brutusandreas/src/pages/home/Home.jsx
@@ -1,6 +1,26 @@
 import React from "react";
+import "./Home.css"; 
 import TopHeader from "../../components/TopHeader";
 import Showcase from "../../components/Showcase";
 import Footer from "../../components/Footer";
 
+const Home = () => {
+  return (
+    <div>
+      <TopHeader />
+      <Showcase />
+      <main>
+        <h1>Welcome to Brutus Andreas Home Page</h1>
+        <p>
+          This is some dummy content for your homepage. You can customize this section with your own information, images, or components.
+        </p>
+        <div>
+          Dummy Box: Add your featured content here!
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
+export default Home;

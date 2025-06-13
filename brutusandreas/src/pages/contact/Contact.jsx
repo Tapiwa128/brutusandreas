de80b206cf383b36fc617eba25eee813577feac5
Tapiwa128 <106350924+Tapiwa128@users.noreply.github.com>
2025-06-13 14:25:56 +0200

    g

--- brutusandreas/src/pages/contact/Contact.jsx
+++ brutusandreas/src/pages/contact/Contact.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Contact.css';
 import Header3 from '../../components/Header3/Header3';
-
+import ContactUs from '../../components/ContactUs/ContactUs';
 function Contact() {
   return (
   <div>
  
  <Header3 />
+ <ContactUs />
 
   </div>
   );

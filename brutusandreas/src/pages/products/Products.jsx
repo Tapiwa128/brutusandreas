211f0ab43f9aa7b4febcbee2784dcd12e5eb0095
Tapiwa128 <106350924+Tapiwa128@users.noreply.github.com>
2025-06-18 16:04:20 +0200

    Update Products.jsx

--- brutusandreas/src/pages/products/Products.jsx
+++ brutusandreas/src/pages/products/Products.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './Products.css';
 import Header2 from '../../components/Header2/Header2';
 import Products1 from '../../components/products1/products1';
+import Dstv from '../../components/Dstv/Dstv';
 import Showcase from '../../components/Showcase/Showcase';
 const Pricing = () => {
     return (
       <div>
       <Header2/>
       <Products1/>
+      <Dstv/>
       <Showcase/>
 
       </div>
